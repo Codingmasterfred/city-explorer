a56518925bb1c0418a2c01b65c0cb6e414d537ac
Codingmasterfred <fredrickg777@gmail.com>
2023-05-08 11:43:11 -0700

    Finish

---
+++ README.md
@@ -0,0 +1,46 @@
+# Project Name : city-explorer
+
+**Author**: Fredrick Gentry
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
+This application will allow an user to search the name of an location and receive the exact longitude and latitude, perdicted weather, and movies relation to that location. It could help someone gather information on an area or use as a tool when they are lost.
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+they have to create a react-creat app, find api's that could help you attain the information needed, and implement the api's in your app
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+build a create-react-app and using Javascript, JSX , and might use a little CSS.
+I will be using yelp,weatherbit,TMDB, and location api's.
+Finally, Upload it to netify after pushing all changes.
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+Time Estimates
+For each of the lab features, make an estimate of the time it will take you to complete the feature, and record your start and finish times for that feature:
+
+Name of feature: _Set up react app_______________________________
+
+Estimate of time needed to complete: 5 min_____
+
+Start time: __11:24___
+
+Finish time: _11:28____
+
+
+Name of feature: _______________________________
+
+Estimate of time needed to complete: ____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ src/LocationiQ.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+//  axios library is required to access LocationIQ, which in turn is used to access the map component.
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+// in order to use the differents states to make changes or updates
+import Card from "react-bootstrap/Card"
+
+function GetLocationData() {
+    const [Change, ChangeFunction] = useState("")
+    // is used to change data from the LocationIQ api the based off the user input
+    const [image, imageFunction] = useState({})
+    // is used to change the image based off the data recieved from the LocationIQ api
+    const [errorMessage,errorMessageFunction] = useState({})
+    // is used to puts the error message into an object data type 
+    
+    let FetchData = async (event) => {
+        try {event.preventDefault()
+        let response = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.858228159b59b411fc2826e2fbfea459&q=${Change}&format=json`)
+        //When used with an async function, await pauses the execution of the function until the .get execute which get information from the server to the clients . 
+        console.log(response)
+        imageFunction(response.data[0])
+        // sets the value of the image from the state based off the input
+        errorMessageFunction({})
+        
+    }
+        catch(error){
+            errorMessageFunction(error)
+            // if the response goes wrong then the error will appear inside of the object data structure
+            console.log(error)
+        }
+        // The try...catch block works by wrapping the code that may potentially throw an error in the try block, and catching any errors in the catch block. This allows for more graceful handling of errors, preventing the component from crashing and providing the opportunity to handle the error in a more controlled manner.
+    }
+    let ImageView = image.lon && image.lat ? `https://maps.locationiq.com/v3/staticmap?key=pk.858228159b59b411fc2826e2fbfea459&center=${image.lat},${image.lon}&zoom=12`: ""
+
+    function onchange(event) {
+        // onchange will update value as it changes 
+        ChangeFunction(event.target.value)
+    }
+    return (
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onChange={onchange} type="text" placeholder="Type A Location" />
+                    {/* form.control asts as a input and as the input is typing it changes */}
+                </Form.Group>
+                <button onClick={FetchData}></button>
+                {/* in order to recieve the data you will have to click this button */}
+            </Form>
+
+            <Card style={{ width: '450px', height: '450px' }} id='card'>
+                <Card.Body style={{ backgroundColor: '#2E7D32'}}>
+                    <Card.Title className='cityInfo'>{image.display_name}</Card.Title>
+                    <Card.Text id='lonAndlad'>
+                        <span className='cityInfo'>Longitude: {image.lon}</span>
+                        <span className='cityInfo'>Latitude: {image.lat}</span>
+                        {errorMessage.message}
+                    </Card.Text> 
+                        <img src={ImageView} ></img>
+                </Card.Body>
+            </Card>
+        </div >
+
+    )
+
+}
+
+
+export default GetLocationData

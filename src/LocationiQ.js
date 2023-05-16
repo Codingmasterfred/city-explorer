40dd0198136591445364ea288c4ff98ed29bc10f
Codingmasterfred <fredrickg777@gmail.com>
2023-05-16 07:29:41 -0700

    frontend

--- src/LocationiQ.js
+++ src/LocationiQ.js
@@ -13,6 +13,9 @@ function GetLocationData() {
     // is used to change the image based off the data recieved from the LocationIQ api
     const [errorMessage, errorMessageFunction] = useState({})
     // is used to puts the error message into an object data type 
+    const [errorMessageforbackend, errorMessageFunctionforbackend] = useState({})
+
+    const[displayweather,displayweatherfunction] = useState([])
 
     let FetchData = async (event) => {
         try {
@@ -22,17 +25,35 @@ function GetLocationData() {
             //When used with an async function, await pauses the execution of the function until the .get execute which get information from the server to the clients . 
             console.log(response)
             imageFunction(response.data[0])
+            // displayweatherfunction()
             // sets the value of the image from the state based off the input
              errorMessageFunction({})
+             FetchData2()
 
         }
         catch (error) {
+            errorMessageFunctionforbackend(error)
             errorMessageFunction(error)
             // if the response goes wrong then the error will appear inside of the object data structure
             console.log(error)
         }
         // The try...catch block works by wrapping the code that may potentially throw an error in the try block, and catching any errors in the catch block. This allows for more graceful handling of errors, preventing the component from crashing and providing the opportunity to handle the error in a more controlled manner.
     }
+    let FetchData2 = async (event) => {
+       
+        try{
+            let result = await axios.get(`http://localhost:3001/weather?searchQuery=${Change}` )
+            console.log(result)
+            displayweatherfunction(result.data)
+    }
+        catch(error){
+
+            console.log(error)
+        }                                 
+
+    }
+
+
     let ImageView = image.lon && image.lat ? `https://maps.locationiq.com/v3/staticmap?key=pk.858228159b59b411fc2826e2fbfea459&center=${image.lat},${image.lon}&zoom=12` : ""
 
     function onchange(event) {
@@ -53,6 +74,14 @@ function GetLocationData() {
 
             <Card style={{ width: '450px', height: '400px' }} id='card'>
                 <Card.Body style={{ backgroundColor: '#2E7D32' }}>
+                    <div>{displayweather.map(arr => {
+                       return (<div> 
+                        <p>{arr.description}</p>
+                        <p>{arr.date}</p>
+                        <p>{arr.lat}</p> 
+                        <p>{arr.lon}</p>
+                        </div>)
+                    })}</div>
                     <Card.Title className='cityInfo'>{image.display_name}</Card.Title>
                     <Card.Text id='lonAndlad'>
                         <span className='cityInfo'>Longitude: {image.lon}</span>

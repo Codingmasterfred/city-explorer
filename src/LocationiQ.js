d2f19d42e3bf10b945b6c6a61171f0ad211ee908
Codingmasterfred <fredrickg777@gmail.com>
2023-05-17 11:20:02 -0700

    frontend

--- src/LocationiQ.js
+++ src/LocationiQ.js
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import { useState } from "react";
 // in order to use the differents states to make changes or updates
 import Card from "react-bootstrap/Card"
+import Table from 'react-bootstrap/Table';
 
 function GetLocationData() {
     const [Change, ChangeFunction] = useState("")
@@ -15,7 +16,7 @@ function GetLocationData() {
     // is used to puts the error message into an object data type 
     const [errorMessageforbackend, errorMessageFunctionforbackend] = useState({})
 
-    const[displayweather,displayweatherfunction] = useState([])
+    const [displayweather, displayweatherfunction] = useState([])
 
     let FetchData = async (event) => {
         try {
@@ -27,8 +28,8 @@ function GetLocationData() {
             imageFunction(response.data[0])
             // displayweatherfunction()
             // sets the value of the image from the state based off the input
-             errorMessageFunction({})
-             FetchData2()
+            errorMessageFunction({})
+            FetchData2()
 
         }
         catch (error) {
@@ -40,16 +41,19 @@ function GetLocationData() {
         // The try...catch block works by wrapping the code that may potentially throw an error in the try block, and catching any errors in the catch block. This allows for more graceful handling of errors, preventing the component from crashing and providing the opportunity to handle the error in a more controlled manner.
     }
     let FetchData2 = async (event) => {
-       
-        try{
-            let result = await axios.get(`http://localhost:3001/weather?searchQuery=${Change}` )
+        // create a new async function named FetchData2
+
+        try {
+            let result = await axios.get(`http://localhost:3001/weather?searchQuery=${Change}`)
+            // await with the async function wait for the promise to return before continuing rendering the rest of the code 
+            //axios.get() method is used to get 
             console.log(result)
             displayweatherfunction(result.data)
-    }
-        catch(error){
+        }
+        catch (error) {
 
             console.log(error)
-        }                                 
+        }
 
     }
 
@@ -65,32 +69,46 @@ function GetLocationData() {
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control style = {{textAlign : "center" , margin: "10px"}} onChange={onchange} type="text" placeholder="Type A Location" />
+                    <Form.Control style={{ textAlign: "center", margin: "10px" }} onChange={onchange} type="text" placeholder="Type A Location" />
                     {/* form.control asts as a input and as the input is typing it changes */}
-                <button onClick={FetchData}>Explore!!</button>
+                    <button onClick={FetchData}>Explore!!</button>
                 </Form.Group>
                 {/* in order to recieve the data you will have to click this button */}
             </Form>
 
             <Card style={{ width: '450px', height: '400px' }} id='card'>
                 <Card.Body style={{ backgroundColor: '#2E7D32' }}>
-                    <div>{displayweather.map(arr => {
-                       return (<div> 
-                        <p>{arr.description}</p>
-                        <p>{arr.date}</p>
-                        <p>{arr.lat}</p> 
-                        <p>{arr.lon}</p>
-                        </div>)
-                    })}</div>
                     <Card.Title className='cityInfo'>{image.display_name}</Card.Title>
                     <Card.Text id='lonAndlad'>
                         <span className='cityInfo'>Longitude: {image.lon}</span>
                         <span className='cityInfo'>Latitude: {image.lat}</span>
                         {errorMessage.message}
                     </Card.Text>
-                    {<img id="Map" src={ImageView} style={{display: ImageView ? "block" : "none" , width: "100%"}} ></img>}
+                    {<img id="Map" src={ImageView} style={{ display: ImageView ? "block" : "none", width: "100%" }} ></img>}
                 </Card.Body>
-            </Card> 
+            </Card>
+                    <div>{displayweather.map(arr => {
+                        return (<div>
+            <Table id="table" striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Location</th>
+                        <th>Weather</th>
+                        <th>Longitude</th>
+                        <th>Latitude</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{Change}</td>
+                        <td>{arr.description}</td>
+                        <td>{arr.lon}</td>
+                        <td>{arr.lat}</td>
+                    </tr>
+                </tbody>
+            </Table>
+                        </div>)
+                    })}</div>
         </div >
 
     )
